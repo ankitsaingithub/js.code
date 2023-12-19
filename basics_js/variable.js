cf82e0a550f82215f0b071d0c2cd9a81fb472a9b
ankitsaingithub <123162729+ankitsaingithub@users.noreply.github.com>
2023-12-19 14:11:28 +0000

    container confing done

---
+++ basics_js/variable.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "ankit@google.com"
+var accountPassword = "2346"
+accountCity = "Alwar"
+
+//accountId = 2 // not allowed bcz it's constant
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity]);
