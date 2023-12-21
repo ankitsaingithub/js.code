b3e815373171023531cbdc87351d35a2e78ed1ef
ankitsaingithub <123162729+ankitsaingithub@users.noreply.github.com>
2023-12-21 13:15:16 +0000

    container file change

---
+++ basics_js/conversion.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+
+let logedIn = "ankit"
+
+let booleanlogedIn = Boolean(logedIn)
+console.log(booleanlogedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"ankit" = true
+*/
+
